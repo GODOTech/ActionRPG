a63ef397a46ff6c41c81cb1694f66cbaaef4d574
GODOTech <Prattz440@gmail.com>
2024-07-27 16:17:16 -0300

    first enemy

---
+++ scenes/enemies/slime.gd
@@ -0,0 +1,35 @@
+class_name  enemy
+extends CharacterBody2D
+@export var speed = 10
+@export var limit = 0.5
+@export var endPoint: Marker2D
+@onready var animation = $AnimatedSprite2D
+
+var startPosition
+var endPosition
+
+func _ready():
+	startPosition = position
+	endPosition = endPoint.global_position
+
+func changeDirection():
+	var tempEnd = endPosition
+	endPosition = startPosition
+	startPosition = tempEnd
+func updateVelocity():
+	var moveDirection = (endPosition - position)
+	if moveDirection.length() < limit:
+		changeDirection()
+	velocity = moveDirection.normalized() * speed
+	
+func updateAnimation():
+	var animationString = "walkUp"
+	if velocity.y > 0:
+		animationString = "walkDown"
+		
+	animation.play(animationString)
+func _physics_process(delta):
+	updateVelocity()
+	move_and_slide()
+	updateAnimation()
+	
