5aab5085203a99c22cfb1c9df2ba597b310cf391
GODOTech <Prattz440@gmail.com>
2024-09-22 11:27:58 -0300

    Pause and inventory_gui

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
-@onready var heartsContainer = $CanvasLayer/heartsContainer # el contenedor de corazones es este hijo
+@onready var heartsContainer = $HUD/heartsContainer # el contenedor de corazones es este hijo
 @onready var player = $player
 
 func _ready():
 	heartsContainer.setMaxHearts(player.maxHealth) # mandarle esta funcion funcion de este hijo: esta variable de este hijo 
 	heartsContainer.updateHearts(player.currentHealth) # actualizar los corazones con la salud del personaje
 	player.healthChanged.connect(heartsContainer.updateHearts) # conectar la señal salud cambio, con actualizar corazones
+
+
+func _on_inventory_gui_closed():
+	get_tree().paused = false
+
+
+func _on_inventory_gui_opened():
+	get_tree().paused = true
